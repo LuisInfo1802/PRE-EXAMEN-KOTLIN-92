d820940f4a6ac06355de05d90fc3ff0f94cb81d4
LuisInfo1802 <luissanchezinfo18@gmail.com>
2023-05-28 22:55:30 -0700

    Se configuración los valúes y se cargó la imagen en los recursos R. Y Se agrego la actividad y se diseñó el layout de Activity_Recibo

---
+++ app/src/main/java/com/example/pre_examen_kotlin_92/Recibo_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.pre_examen_kotlin_92
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Recibo_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recibo)
+    }
+}
